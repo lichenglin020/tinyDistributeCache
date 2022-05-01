97499ab09b737c2878a24eb6d91ff7139b324cb8
chenglinli <281913796@qq.com>
2022-05-01 17:55:11 +0800

    修订Cmakelists.txt，支持多main项目结构；添加.gitignore文件；添加alicloud环境依赖

--- test/test_thread_pool.cpp
+++ test/test_thread_pool.cpp
@@ -32,9 +32,9 @@ namespace{
         }
     }
 }
-//int main(int argc, char** argv) {
-//    ::testing::InitGoogleTest(&argc, argv);
-//    return RUN_ALL_TESTS();
-//    return 0;
-//}
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+    return 0;
+}
 
