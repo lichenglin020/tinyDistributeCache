cab1f6dff7550d7833cc52d4f42a020f4ac2d6f5
chenglinli <281913796@qq.com>
2022-05-09 20:48:33 +0800

    添加独立的JOSN格式类，进一步降低代码耦合，方便JSON格式修订

---
+++ cache/HttpJson.cpp
@@ -0,0 +1,53 @@
+//
+// Created by Chenglinli on 2022/5/9.
+//
+
+#include "HttpJson.h"
+
+/**
+ * 对收到的数据构建JSON数据
+ * @param js
+ * @param buffer
+ * @return 返回是否构建成功
+ */
+bool HttpJson::getRequestJson(json &js, char *buffer) {
+    if(json::accept(buffer)){
+        js = json::parse(buffer);
+        return true;
+    }else{
+        return false;
+    }
+}
+
+/**
+ * 获取发送至备份服务器的JSON数据
+ * @param key
+ * @param value
+ * @return
+ */
+json HttpJson::writeBackupJson(const std::string &key, const std::string &value) {
+    json writeBackupJson, data;
+    writeBackupJson["type"] = KEY_VALUE_WRITE_BK;
+    data["flag"] = true;
+    data["key"] = key;
+    data["value"] = value;
+    writeBackupJson["data"] = data;
+    return writeBackupJson;
+}
+
+/**
+ * 构建返回给客户端查询结果的JSON数据
+ * @param key
+ * @param value
+ * @return
+ */
+json HttpJson::respondClientJson(const std::string &key, const std::string &value) {
+    json respondClientJson, data;
+    respondClientJson["type"] = KEY_VALUE_READ;
+    data["flag"] = !value.empty();
+    data["key"] = key;
+    data["value"] = value;
+    respondClientJson["data"] = data;
+    return respondClientJson;
+}
+
---
+++ cache/HttpJson.h
@@ -0,0 +1,26 @@
+//
+// Created by Chenglinli on 2022/5/9.
+//
+
+#ifndef TINYDISTRIBUTECACHE_HTTPJSON_H
+#define TINYDISTRIBUTECACHE_HTTPJSON_H
+#include "../common/json.hpp"
+using json = nlohmann::json;
+
+#define KEY_VALUE_READ 0      // 客户端读取数据请求标志位
+#define KEY_VALUE_WRITE 1     // 客户端写入数据请求标志位
+#define KEY_VALUE_WRITE_BK 2  // 向备份节点同步数据的标志位
+
+/**
+ * 定义通信的Json格式
+ */
+
+class HttpJson {
+public:
+    static bool getRequestJson(json& js, char* buffer);
+    static json writeBackupJson(const std::string& key, const std::string& value);
+    static json respondClientJson(const std::string &key, const std::string &value);
+};
+
+
+#endif //TINYDISTRIBUTECACHE_HTTPJSON_H
